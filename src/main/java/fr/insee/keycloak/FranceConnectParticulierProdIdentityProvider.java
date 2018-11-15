68489036c73c5dcbbdd691918e6c42bef6c56d0f
Cédric Couralet <cedric.couralet@insee.fr>
2018-11-15 18:40:18 +0100

    Add Prod provider

---
+++ src/main/java/fr/insee/keycloak/FranceConnectParticulierProdIdentityProvider.java
@@ -0,0 +1,20 @@
+package fr.insee.keycloak;
+
+import org.keycloak.broker.oidc.OIDCIdentityProviderConfig;
+import org.keycloak.models.KeycloakSession;
+
+public class FranceConnectParticulierProdIdentityProvider extends FranceConnectIdentityProvider {
+
+
+  protected String authorizationUrl = "https://app.franceconnect.gouv.fr/api/v1/authorize";
+  protected String tokenUrl = "https://app.franceconnect.gouv.fr/api/v1/token";
+  protected String userInfoUrl = "https://app.franceconnect.gouv.fr/api/v1/userinfo";
+  protected String logoutUrl = "https://app.franceconnect.gouv.fr/api/v1/logout";
+
+
+  public FranceConnectParticulierProdIdentityProvider(KeycloakSession session,
+      OIDCIdentityProviderConfig config) {
+    super(session, config);
+  }
+
+}
---
+++ src/main/java/fr/insee/keycloak/FranceConnectParticulierProdIdentityProviderFactory.java
@@ -0,0 +1,30 @@
+package fr.insee.keycloak;
+
+import org.keycloak.broker.oidc.OIDCIdentityProviderConfig;
+import org.keycloak.broker.provider.AbstractIdentityProviderFactory;
+import org.keycloak.broker.social.SocialIdentityProviderFactory;
+import org.keycloak.models.IdentityProviderModel;
+import org.keycloak.models.KeycloakSession;
+
+public class FranceConnectParticulierProdIdentityProviderFactory
+    extends AbstractIdentityProviderFactory<FranceConnectIdentityProvider>
+    implements SocialIdentityProviderFactory<FranceConnectIdentityProvider> {
+
+  @Override
+  public String getName() {
+    return "France Connect Particulier (Production)";
+  }
+
+  @Override
+  public FranceConnectParticulierTestIdentityProvider create(KeycloakSession session,
+      IdentityProviderModel model) {
+    return new FranceConnectParticulierTestIdentityProvider(session,
+        new OIDCIdentityProviderConfig(model));
+  }
+
+  @Override
+  public String getId() {
+    return "franceconnect-particulier";
+  }
+
+}
