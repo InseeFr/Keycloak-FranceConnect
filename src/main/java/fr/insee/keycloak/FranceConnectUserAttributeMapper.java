04cfa379f0485a1dce532603a91d27c6b6cc941c
Cédric Couralet <cedric.couralet@insee.fr>
2019-02-15 07:48:03 +0100

    Change the inherited mapper to UserAttributeMapper.
    This allowed to define the mappers to set the value of firstName and
    lastName.
    It also allows to update profile information from FranceConnect without
    needing to delete the keycloak user.

--- src/main/java/fr/insee/keycloak/FranceConnectUserAttributeMapper.java
+++ src/main/java/fr/insee/keycloak/FranceConnectUserAttributeMapper.java
@@ -1,18 +1,22 @@
 package fr.insee.keycloak;
 
-import org.keycloak.broker.oidc.mappers.AbstractJsonUserAttributeMapper;
+import org.keycloak.broker.oidc.mappers.UserAttributeMapper;
 
-public class FranceConnectUserAttributeMapper extends AbstractJsonUserAttributeMapper {
+public class FranceConnectUserAttributeMapper extends UserAttributeMapper {
   private static final String[] cp =
       new String[] {
           FranceConnectParticulierProdIdentityProviderFactory.PROVIDER_ID,
           FranceConnectParticulierTestIdentityProviderFactory.PROVIDER_ID};
 
+  
+  
+  
   @Override
   public String[] getCompatibleProviders() {
     return cp;
   }
 
+  
   @Override
   public String getId() {
     return "franceconnect-user-attribute-mapper";
