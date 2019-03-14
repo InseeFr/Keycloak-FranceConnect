3425f109b3302ea7fa55c7dd77b451cfa7ee1143
Cédric Couralet <cedric.couralet@insee.fr>
2019-03-14 18:12:47 +0100

    Fix wrong url

--- src/main/java/fr/insee/keycloak/FranceConnectParticulierProdIdentityProvider.java
+++ src/main/java/fr/insee/keycloak/FranceConnectParticulierProdIdentityProvider.java
@@ -6,12 +6,6 @@ import org.keycloak.models.KeycloakSession;
 public class FranceConnectParticulierProdIdentityProvider extends FranceConnectIdentityProvider {
 
 
-  protected String authorizationUrl = "https://app.franceconnect.gouv.fr/api/v1/authorize";
-  protected String tokenUrl = "https://app.franceconnect.gouv.fr/api/v1/token";
-  protected String userInfoUrl = "https://app.franceconnect.gouv.fr/api/v1/userinfo";
-  protected String logoutUrl = "https://app.franceconnect.gouv.fr/api/v1/logout";
-
-
   public FranceConnectParticulierProdIdentityProvider(KeycloakSession session,
       OIDCIdentityProviderConfig config) {
     super(session, config);
