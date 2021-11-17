d76ce0a33775cef125b0ed967ae4a101026ea5a5
Mathieu Boisnard <mathieu.boisnard@lacombedulionvert.fr>
2021-11-17 13:29:10 +0100

    Update maven plugin versions & maven wrapper version
    Signed-off-by: Mathieu Boisnard <mathieu.boisnard@lacombedulionvert.fr>

--- src/test/java/fr/insee/keycloak/utils/MockAuthenticationSessionModel.java
+++ src/test/java/fr/insee/keycloak/utils/MockAuthenticationSessionModel.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- *
+ * Creates a fake implementation of AuthenticationSessionModel with clientNotes map
  */
 public final class MockAuthenticationSessionModel implements AuthenticationSessionModel {
 
