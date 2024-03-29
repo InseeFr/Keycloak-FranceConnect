33ea0d215c7a7e16c9d8773c0af6ddf218959cca
Laurent Meunier <lme@atolcd.com>
2024-03-29 16:44:10 +0100

    Add EIDAS level and FranceConnect environment options in admin console
    fix #74
    
    Signed-off-by: Laurent Meunier <lme@atolcd.com>

--- src/main/java/fr/insee/keycloak/providers/agentconnect/AgentConnectIdentityProviderConfig.java
+++ src/main/java/fr/insee/keycloak/providers/agentconnect/AgentConnectIdentityProviderConfig.java
@@ -1,10 +1,15 @@
 package fr.insee.keycloak.providers.agentconnect;
 
 import fr.insee.keycloak.providers.common.AbstractBaseProviderConfig;
+import fr.insee.keycloak.providers.common.EidasLevel;
 import org.keycloak.models.IdentityProviderMapperModel;
 import org.keycloak.models.IdentityProviderModel;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static fr.insee.keycloak.providers.agentconnect.AgentConnectIdentityProviderFactory.AC_PROVIDER_MAPPERS;
 import static fr.insee.keycloak.providers.agentconnect.AgentConnectIdentityProviderFactory.DEFAULT_AC_ENVIRONMENT;
@@ -33,5 +38,32 @@ final class AgentConnectIdentityProviderConfig extends AbstractBaseProviderConfi
   @Override
   protected List<IdentityProviderMapperModel> getDefaultMappers() {
     return AC_PROVIDER_MAPPERS;
+  }
+
+  public static List<ProviderConfigProperty> getConfigProperties() {
+    List<String> environments = Stream.of(ACEnvironment.values())
+        .map(Enum::name)
+        .collect(Collectors.toList());
+
+    List<String> eidasLevels = Stream.of(EidasLevel.values())
+        .map(Enum::name)
+        .collect(Collectors.toList());
+
+    return ProviderConfigurationBuilder.create()
+        .property().name(ACEnvironment.ENVIRONMENT_PROPERTY_NAME)
+        .label("Environnement FranceConnect")
+        .helpText("Permet de choisir l'environnement FranceConnect. Effet : change les urls vers FranceConnect.")
+        .type(ProviderConfigProperty.LIST_TYPE)
+        .options(environments)
+        .defaultValue(DEFAULT_AC_ENVIRONMENT)
+        .add()
+        .property().name(EidasLevel.EIDAS_LEVEL_PROPERTY_NAME)
+        .label("Niveau de garantie eIDAS")
+        .helpText("Permet de fixer le niveau de garantie du compte utilisateur souhaité. Effet : désactive des fournisseurs d'identités (FI) sur la page de login FranceConnect.\"")
+        .type(ProviderConfigProperty.LIST_TYPE)
+        .options(eidasLevels)
+        .defaultValue(EidasLevel.EIDAS1)
+        .add()
+        .build();
   }
 }
