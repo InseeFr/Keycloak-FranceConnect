8a59262f834daddfd6d39a63ee7d608332d481f0
Laurent Meunier <lme@atolcd.com>
2026-01-08 10:58:05 +0100

    [FIX] missing birthplace claim for users not born in France
    Signed-off-by: Laurent Meunier <lme@atolcd.com>

--- src/main/java/fr/insee/keycloak/providers/franceconnect/IdentitePivot.java
+++ src/main/java/fr/insee/keycloak/providers/franceconnect/IdentitePivot.java
@@ -14,6 +14,9 @@ public class IdentitePivot {
   static final public String CLAIM_BIRTHPLACE = "birthplace";
   static final public String CLAIM_BIRTHCOUNTRY = "birthcountry";
 
+  // https://www.insee.fr/fr/metadonnees/geographie/pays/99100-france
+  static final private String INSEE_CODE_FRANCE = "99100";
+
   private final Map<String, String> attributeMapping;
   private final Map<String, List<String>> attributes;
 
@@ -27,22 +30,30 @@ public class IdentitePivot {
     if (attributeName == null) {
       throw new RuntimeException(String.format("Missing attribute mapper for claim '%s'", claimName));
     }
-    String attributeValue = this.attributes.get(attributeName).get(0);
-    if (attributeValue == null) {
-      throw new RuntimeException(String.format("Missing attribute value for claim '%s' / attribute '%s'", claimName, attributeName));
+    List<String> attributeValues = this.attributes.get(attributeName);
+    if (attributeValues == null) {
+      throw new RuntimeException(String.format(" Missing attribute value for claim '%s' / attribute '%s'", claimName, attributeName));
     }
-    return entry(attributeName, attributeValue);
+    return entry(attributeName, attributeValues.get(0));
   }
 
   public Map<String, String> toMap() {
-    return Map.ofEntries(
+    Map<String, String> attributes = new java.util.HashMap<>(Map.ofEntries(
         resolve(CLAIM_GIVEN_NAME),
         resolve(CLAIM_FAMILY_NAME),
         resolve(CLAIM_GENDER),
         resolve(CLAIM_BIRTHDATE),
-        resolve(CLAIM_BIRTHPLACE),
         resolve(CLAIM_BIRTHCOUNTRY)
-    );
+    ));
+
+    // add "birthplace" attribute, only for users not born in France
+    // see description for the "birthplace" claim: https://docs.partenaires.franceconnect.gouv.fr/fi/general/donnees-utilisateur/#l-identite-pivot
+    if (INSEE_CODE_FRANCE.equals(resolve(CLAIM_BIRTHCOUNTRY).getValue())) {
+      Map.Entry<String, String> birthPlace = resolve(CLAIM_BIRTHPLACE);
+      attributes.put(birthPlace.getKey(), birthPlace.getValue());
+    }
+
+    return attributes;
   }
 
   @Override
